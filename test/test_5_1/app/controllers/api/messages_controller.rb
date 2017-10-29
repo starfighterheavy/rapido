5b067d79a148c8aa7aa20b9be1a7d26637e2c1b7
Jonathan Kirst <jskirst@gmail.com>
2017-10-29 12:42:22 -0700

    Refactor (#7)
    * Refactor
    
    * Rubocopping

--- test/test_5_1/app/controllers/api/messages_controller.rb
+++ test/test_5_1/app/controllers/api/messages_controller.rb
@@ -2,10 +2,7 @@ class Api::MessagesController < ActionController::API
   include Rapido::Controller
   include Rapido::ApiController
 
-  owner_class :comlink
-  owner_lookup_param :comlink_token
-  owner_lookup_field :token
-
+  belongs_to :comlink, foreign_key: :token
   free_from_authority!
   permit_all_params!
 end
--- test/test_5_1/app/controllers/hydrospanners_controller.rb
+++ test/test_5_1/app/controllers/hydrospanners_controller.rb
@@ -1,16 +1,14 @@
 class HydrospannersController < ApplicationController
   include Rapido::AppViews
 
+  belongs_to :toolbox, foreign_key: :name
   attr_permitted :name
   lookup_param :name
-
-  owner_class :toolbox
-  owner_lookup_param :toolbox_name
-  owner_lookup_field :name
+  authority :current_user_account
 
   private
 
-  def authority
+  def current_user_account
     current_user.account
   end
 end
