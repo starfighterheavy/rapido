c0f318424b99cae8759ee7558c9602fea9b91831
Jonathan Kirst <jskirst@gmail.com>
2017-11-06 07:17:09 -0800

    Belongs to nothing (#9)
    Controller now has belongs_to_nothing option for root objects, rather than belonging to authority.
    Controller now has has_many option, which is used in views to link out to owned resource collections.
    Views adjusted to make them a bit easier to work with

--- test/test_5_1/app/controllers/toolboxes_controller.rb
+++ test/test_5_1/app/controllers/toolboxes_controller.rb
@@ -1,7 +1,11 @@
 class ToolboxesController < ApplicationController
+  include Rapido::AppViews
   before_action :authenticate_user!
 
-  belongs_to :account, getter: :current_user_account
+  belongs_to_nothing!
+  has_many :hydrospanners
+
+  authority :current_user_account
   attr_permitted :name
   lookup_param :name
 end
