a30a0c83735ec5ad60f21e3f54b165d3abc65e49
Jonathan Kirst <jskirst@gmail.com>
2019-04-03 10:49:18 -0700

    wip

--- lib/rapido/api_controller.rb
+++ lib/rapido/api_controller.rb
@@ -24,30 +24,75 @@ module Rapido
     end
 
     def create
+      before_build
       new_resource = build_resource(resource_params)
+      before_create
       if new_resource.save
+        after_create_success
+        return if performed?
         render json: present_resource(new_resource), status: :created
       else
+        after_create_failure
+        return if performed?
         render json: { errors: new_resource.errors.full_messages }, status: 422
       end
     end
 
     def destroy
       resource_before_destruction = present_resource(resource)
+      before_destroy
       resource.destroy
+      after_destroy_success
+      return if performed?
       render json: resource_before_destruction
     end
 
     def update
+      before_assign_attributes
       resource.assign_attributes(resource_params)
+      before_update
       if resource.save
+        after_update_success
+        return if performed?
         render json: present_resource(resource)
       else
+        after_update_failure
+        return if performed?
         render json: { errors: resource.errors.full_messages }, status: 422
       end
     end
 
     private
+
+    def after_create_failure
+    end
+
+    def after_create_success
+    end
+
+    def after_destroy_success
+    end
+
+    def after_update_failure
+    end
+
+    def after_update_success
+    end
+
+    def before_assign_attributes
+    end
+
+    def before_build
+    end
+
+    def before_create
+    end
+
+    def before_destroy
+    end
+
+    def before_update
+    end
 
     def present_resource(resource)
       args = presenter_args.nil? ? nil : presenter_args.map { |arg| params[arg] }
