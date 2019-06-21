dfa07d63b9dd690397b09ab67e0779c50a4938c2
Jonathan Kirst <jskirst@gmail.com>
2019-06-21 12:29:03 -0700

    Bumping and bug fix for v0.7

--- test/dummy/app/controllers/api/messages_controller.rb
+++ test/dummy/app/controllers/api/messages_controller.rb
@@ -2,11 +2,11 @@ class Api::MessagesController < Api::ApplicationController
   include Rapido::Controller
   include Rapido::ApiController
 
+  attr_permitted :content
+
   skip_before_action :load_authority
 
   belongs_to :comlink, foreign_key: :token
-
-  permit_all_params!
 
   presented_by Api::MessagePresenter
 
